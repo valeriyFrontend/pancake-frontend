ad75b8f3de79dfcd2a3a85a10c742c79cc5369c1
Valeriy Kovalenko <57315743+valeriyFrontend@users.noreply.github.com>
2025-07-18 16:03:11 +0300

    Update _document.tsx

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -37,6 +37,19 @@ class MyDocument extends Document {
           {process.env.NEXT_PUBLIC_NODE_PRODUCTION && (
             <link rel="preconnect" href={process.env.NEXT_PUBLIC_NODE_PRODUCTION} />
           )}
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MSPCRZNB');
+              `,
+            }}
+          />
+          {/* End Google Tag Manager */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href={FARMS_API} />
           <link
@@ -48,14 +61,16 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTAG}`}
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MSPCRZNB"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
-            />
+            ></iframe>
           </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
           <div id="portal-root" />
